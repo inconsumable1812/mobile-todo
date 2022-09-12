70f84ce697aee3867cce4e9c0076284aefdac50f
Inconsumable <inconsumable1812@gmail.com>
2022-09-12 11:39:53 +0800

    add loader

--- src/features/todo/Todo.tsx
+++ src/features/todo/Todo.tsx
@@ -1,5 +1,7 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { StyleSheet, ActivityIndicator, View } from 'react-native';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { MAIN_BUTTON_COLOR } from './constants';
 import { selectTodo, setTodosFromStorage } from './redux/slice';
 import { getTodosData } from './utils';
 import { TodoList } from './view/components/TodoList/TodoList';
@@ -8,10 +10,12 @@ import { ToggleTodoButton } from './view/components/ToggleTodoButton/ToggleTodoB
 type Props = {};
 
 export const Todo: FC<Props> = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useAppDispatch();
   const storageTodos = getTodosData();
   const getTodosValue = async () => {
     const value = await storageTodos;
+    setIsLoading(false);
     if (value === null) return;
     dispatch(setTodosFromStorage(value));
   };
@@ -38,7 +42,23 @@ export const Todo: FC<Props> = () => {
   return (
     <>
       <ToggleTodoButton />
-      <TodoList todos={filteredTodos}></TodoList>
+      {isLoading && (
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator
+            size="large"
+            color={MAIN_BUTTON_COLOR}
+          ></ActivityIndicator>
+        </View>
+      )}
+      {!isLoading && <TodoList todos={filteredTodos}></TodoList>}
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  loaderContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 0.5
+  }
+});
